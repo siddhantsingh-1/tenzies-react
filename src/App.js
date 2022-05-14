4e9de7633215eb346bf0f21a1778106d2359a424
Siddhant Singh <siddhantsingh@Siddhants-MacBook-Pro.local>
2022-05-15 03:20:10 +0530

    Add timer functionality and remove roll counter from display

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ import Confetti from 'react-confetti'
 
 export default function App() {
 
+    const [ dice, setDice ] = React.useState(allNewDice())
+    const [ tenzies, setTenzies ] = React.useState(false)
+    const [rolls, setRolls] = React.useState(0)
+    const [ pbRolls, setPbRolls ] = React.useState(JSON.parse(localStorage.getItem('pbRolls')) || null)
+    const [time, setTime] = React.useState(0)
+    const [pbTime, setPbTime] = React.useState(JSON.parse(localStorage.getItem('pbTime')) || null)
+    const [gameOn, setGameOn] = React.useState(false)
+
     function generateNewDie() {
         const randomNum = Math.ceil(Math.random()*6)
         return {
@@ -22,11 +30,6 @@ export default function App() {
         return newDie
     }
 
-    const [ dice, setDice ] = React.useState(allNewDice())
-    const [ tenzies, setTenzies ] = React.useState(false)
-    const [rolls, setRolls] = React.useState(0)
-    const [ pbRolls, setPbRolls ] = React.useState(JSON.parse(localStorage.getItem('pbRolls')) || null)
-
     React.useEffect(() => {
         const allHeld = dice.every(die => die.isHeld)
         const allSameValue = dice.every(die => die.value === dice[0].value)
@@ -36,7 +39,7 @@ export default function App() {
     }, [dice])
 
     React.useEffect(() => {
-        if(!pbRolls || pbRolls < rolls) {
+        if(tenzies && (!pbRolls || rolls < pbRolls)) {
             setPbRolls(rolls)
             localStorage.setItem('pbRolls', rolls)
         }
@@ -64,6 +67,47 @@ export default function App() {
         )
     }
 
+    // Timer Functionality:-
+    React.useEffect(() => {
+
+        const noneHeld = dice.every(die => !(die.isHeld))
+
+        if(!noneHeld || rolls === 1) {
+            setGameOn(true)
+        }
+
+        if(rolls === 0) {
+            setTime(0)
+        }
+
+        if(tenzies) {
+            setGameOn(false)
+        }
+
+    }, [rolls, tenzies, dice])
+
+    React.useEffect(() => {
+        let interval = null
+
+        if(gameOn) {
+            interval = setInterval(() => {
+                setTime(prevTime => prevTime + 10)
+            }, 10)
+        } else {
+            if(tenzies && (!pbTime || time < pbTime)) {
+                setPbTime(time)
+                localStorage.setItem('pbTime', time)
+            }
+            clearInterval(interval)
+        }
+
+        return () => clearInterval(interval)
+    }, [gameOn])
+
+    let formattedPbTime = pbTime ? `${("0" + Math.floor(pbTime / 60000) % 60).slice(-2)}:${("0" + Math.floor(pbTime / 1000) % 60).slice(-2)}:${("0" + Math.floor(pbTime / 10) % 100).slice(-2)}` : 'N/A'
+
+
+    // Making Dice JSX Elements:-
     const newDiceElements = dice.map(die => {
         return <Dice value={die.value} key={die.id} id={die.id} holdDice={holdDice} isHeld={die.isHeld} />
     })
@@ -73,8 +117,11 @@ export default function App() {
             {tenzies && <Confetti width={400} height={420} />}
             <h3 className="title">Tenzies</h3>
             <section className="stats">
-                <h4 className="rolls">Rolls: {rolls}</h4>
-                <h4 className="pb-rolls">{pbRolls ? `PB: ${pbRolls} Rolls` : `PB: N/A`}</h4>
+                <h4 className="time">Time: <span>{("0" + Math.floor(time / 60000) % 60).slice(-2)}:</span>
+                    <span>{("0" + Math.floor(time / 1000) % 60).slice(-2)}:</span>
+                    <span>{("0" + Math.floor(time / 10) % 100).slice(-2)}</span>
+                </h4>
+                <h4 className="pb-time">PB: {formattedPbTime}</h4>
             </section>
             <div className="description">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</div>
             <div className="dice-section">
